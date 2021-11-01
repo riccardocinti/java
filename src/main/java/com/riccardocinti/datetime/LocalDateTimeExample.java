3547d157b2c78f4d3c77520cc818ff88e74c0e40
Riccardo <cintiriccardo@gmail.com>
2021-11-01 23:51:38 +0100

    localdatetime examples

--- src/main/java/com/riccardocinti/datetime/LocalDateTimeExample.java
+++ src/main/java/com/riccardocinti/datetime/LocalDateTimeExample.java
@@ -1,5 +1,7 @@
 package com.riccardocinti.datetime;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
 
@@ -11,6 +13,11 @@ public class LocalDateTimeExample {
         System.out.printf("Apr 15, 1994 @ 11:30am: %s\n",
                 LocalDateTime.of(1994, Month.APRIL, 15, 11, 30));
 
+        System.out.printf("6 months from now: %s\n",
+                LocalDateTime.now().plusMonths(6));
+
+        System.out.printf("6 months ago: %s\n",
+                LocalDateTime.now().minusMonths(6));
     }
 
 }
